a3b8a298188629f5eb6fabe374bd6a576a1f213d
Jae <irrisjay@gmail.com>
2019-03-08 14:30:33 +0800

    .~

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var T = _x => _fn_like =>
 		: T (T (_x) (R .head (_fn_like))) (R .tail (_fn_like))
 	:!! (_fn_like .constructor === Function) ? _fn_like (_x)
 	: undefined
+var impure = dirty_f => x => {;dirty_f (x)}
 
 
 
@@ -70,23 +71,23 @@ var respond = _x => {;ctx .body = _x}
 module .exports = require ('koa-qs') (new (require ('koa')))
 	.use (require ('koa-compress') ())
 	.use (require ('koa-cors') ())
-	.use (function (ctx, next) {
-		return next ()
-			.catch (function (err) {;
-				{;console .error (err)}
-				
-				;ctx .type = 'application/json'
-				;ctx .status = /*err .code || */500
-				//;; ctx .message = err .message || 'Internal Server Error'
-				;ctx .body = { error : err .message }
-				if (debug) {
-					;ctx .body .stack = err .stack } }) })
+	.use (impure ((ctx, next) =>
+		next ()
+		.catch ((err) => {;
+			impure ({;console .error (err)}
+			
+			;ctx .type = 'application/json'
+			;ctx .status = /*err .code || */500
+			//;; ctx .message = err .message || 'Internal Server Error'
+			;ctx .body = { error : err .message }
+			if (debug) {
+				;ctx .body .stack = err .stack } }) ))
 	.use (require ('koa-morgan') ('combined'))
 	.use (require ('koa-bodyparser') ({ strict : false }))
 	.use (require ('koa-json') ())
 	.use (require ('koa-router') ()
-	.post ('/any/signup', (ctx, next) => {;
-		return go
+	.post ('/any/signup', impure ((ctx, next) =>
+		go
 		.then (_ => {;
 			var { username, role, email, password, first_name, last_name, gender, age, height, weight, faculty, department } = ctx .request .body
 			
@@ -94,32 +95,32 @@ module .exports = require ('koa-qs') (new (require ('koa')))
 			;return create_client ({ username, password }) })
 		.then (_client => ({ ok : true, client : _client }))
 		.catch (expect_ok)
-		.then (respond) })
-	.post ('/any/login', (ctx, next) => {;
-		return go
+		.then (respond) ))
+	.post ('/any/login', impure ((ctx, next) =>
+		go
 		.then (_ => {;
 			var { username, password } = ctx .request .body
 			
 			;return create_client ({ username, password }) })
 		.then (_client => ({ ok : _client === undefined, client : _client }))
 		.catch (expect_ok)
-		.then (respond) })
-	.get ('/stats', (ctx, next) => {;
-		return go
+		.then (respond) ))
+	.get ('/stats', impure ((ctx, next) =>
+		go
 		.then (_ => {
 			var { client } = ctx .query
 
 			return find_stats (client_user (client)) })
 		.then (_stats => ({ ok : _stats === undefined, stats : _stats }))
 		.catch (expect_ok)
-		.then (respond) })
-	.post ('/stats', (ctx, next) => {;
-		return go
+		.then (respond) ))
+	.post ('/stats', impure ((ctx, next) => 
+		go
 		.then (_ => {
 			var { client, timestamp, distance, calories, steps } = ctx .query
 
 			;return create_stat (client_user (client)) ({ timestamp, distance, calories, steps }) })
 		.then (_ => ({ ok : true }))
 		.catch (expect_ok)
-		.then (respond) })
+		.then (respond) )
 	.routes ())
