a18a283fa6132321339b701163d36cf988d20b42
Jae <irrisjay@gmail.com>
2019-07-26 11:30:12 +0800

    ~o fixed 404

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
+var { R, suppose, equals } = require ('camarche/core')
+var { go, impure, trace, trace_as } = require ('camarche/effects')
 var R = require ('ramda')
 
 var debug = true
 
 
-
-var go = Promise .resolve ()
-var suppose = fn_form => fn_form ()
-var equals = x => y => R .equals (x) (y)
-var impure = dirty_f => (... x) => {;dirty_f (... x)}
 
 
 
@@ -21,9 +18,9 @@ var impure = dirty_f => (... x) => {;dirty_f (... x)}
 var users = []
 var trophies = {}
 
-var create_user = user => {
-	var { username, role, email, password, first_name, last_name, gender, age, height, weight, faculty, department } = user
-	user =
+var create_user = params => {
+	var { username, role, email, password, first_name, last_name, gender, age, height, weight, faculty, department } = params
+	var user =
 	{ username, role, email, password, first_name, last_name, gender, age, height, weight, faculty, department
 	, stats: [] }
 	;users = [ ... users, user ] }
@@ -43,14 +40,15 @@ var find_stats = user =>
 
 var clients = {}
 
-var create_client = ({ username, password }) => {
+var fresh_client = ({ username, password }) => {
 	var _user = find_user ({ username, password })
+	;console .log (_user, users)
 	if (_user) {
 		var _client = uuid ()
 		while (clients [_client]) {;_client = uuid ()}
 		;clients [_client] = { user: _user, ok: false }
 		return _client } }
-var client_user = client => clients [client]
+var client_user_ = client => clients [client]
 
 var uuid = _ =>
 	'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx' .replace (/[xy]/g, c => 
@@ -65,7 +63,7 @@ var expect_ok = _error => {
 	;console .error (_error)
   
 	return { error: 'An unexpected error occured' } }
-var respond = ctx => _x => {;ctx .body = _x}
+var respond = ctx => _response => {;ctx .body = _response}
 
 var server_ = routes =>
 	require ('koa-qs') (new (require ('koa')))
@@ -73,17 +71,17 @@ var server_ = routes =>
 		.use (require ('koa-cors') ())
 		.use (impure ((ctx, next) =>
 			next ()
-			.catch ((err) => {
+			.catch (err => {
 				;console .error (err)
 				
 				;ctx .type = 'application/json'
-				;ctx .status = /*err .code || */500
-				//;; ctx .message = err .message || 'Internal Server Error'
-				;ctx .body = { error : err .message }
+				;ctx .status = 500
+				//;ctx .message = err .message || 'Internal Server Error'
+				;ctx .body = { error: err .message }
 				if (debug) {
-					;ctx .body .stack = err .stack } }) ))
+					;ctx .body .stack = err .stack } } ) ) )
 		.use (require ('koa-morgan') ('combined'))
-		.use (require ('koa-bodyparser') ({ strict : false }))
+		.use (require ('koa-bodyparser') ({ strict: false }))
 		.use (require ('koa-json') ())
 		.use (routes (require ('koa-router') ()) .routes ())
 
@@ -95,8 +93,9 @@ module .exports = server_ (routes => routes
 			var { username, role, email, password, first_name, last_name, gender, age, height, weight, faculty, department } = ctx .request .body
 			
 			;create_user ({ username, role, email, password, first_name, last_name, gender, age, height, weight, faculty, department })
-			;return create_client ({ username, password }) })
-		.then (_client => ({ ok : true, client : _client }))
+			;var _client = fresh_client ({ username, password })
+			return _client } )
+		.then (_client => ({ ok: true, client: _client }))
 		.catch (expect_ok)
 		.then (respond (ctx)) ) )
 	.post ('/any/login', impure ((ctx, next) =>
@@ -104,8 +103,9 @@ module .exports = server_ (routes => routes
 		.then (_ => {
 			var { username, password } = ctx .request .body
 			
-			;return create_client ({ username, password }) })
-		.then (_client => ({ ok : _client === undefined, client : _client }))
+			;var _client = fresh_client ({ username, password })
+			return _client } )
+		.then (_client => ({ ok: equals (_client) (undefined), client: _client }))
 		.catch (expect_ok)
 		.then (respond (ctx)) ) )
 	.get ('/stats', impure ((ctx, next) =>
@@ -113,8 +113,8 @@ module .exports = server_ (routes => routes
 		.then (_ => {
 			var { client } = ctx .query
 
-			return find_stats (client_user (client)) })
-		.then (_stats => ({ ok : _stats === undefined, stats : _stats }))
+			return find_stats (client_user_ (client)) } )
+		.then (_stats => ({ ok: (_stats) (undefined), stats: _stats }))
 		.catch (expect_ok)
 		.then (respond (ctx)) ) )
 	.post ('/stats', impure ((ctx, next) => 
@@ -122,8 +122,8 @@ module .exports = server_ (routes => routes
 		.then (_ => {
 			var { client, timestamp, distance, calories, steps } = ctx .request .body
 
-			;return create_stat (client_user (client)) ({ timestamp, distance, calories, steps }) })
-		.then (_ => ({ ok : true }))
+			;create_stat (client_user_ (client)) ({ timestamp, distance, calories, steps }) } )
+		.then (_ => ({ ok: true }))
 		.catch (expect_ok)
 		.then (respond) ) )
 	.post ('/update', impure ((ctx, next) =>
@@ -137,8 +137,9 @@ module .exports = server_ (routes => routes
 				current_user [attr] = ctx .request .body [attr] }
 
 			;create_user ({ username, role, email, password, first_name, last_name, gender, age, height, weight, faculty, department })
-			;return create_client ({ username, password }) })
-		.then (_client => ({ ok : true, client : _client }))
+			;var _client = fresh_client ({ username, password })
+			return _client } )
+		.then (_client => ({ ok: true, client: _client }))
 		.catch (expect_ok)
 		.then (respond (ctx)) ) )
 	)
